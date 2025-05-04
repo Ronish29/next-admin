6814264c698be9ea37c39acd44936355ba4b6ce5
Ronish29 <ronishparmar2003@gmail.com>
2025-05-04 15:21:12 +0530

    setup done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { SidebarProvider } from "@/admin-context/SidebarContext";
+import { ThemeProvider } from "@/admin-context/ThemeContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <SidebarProvider>
+        <Component {...pageProps} />
+      </SidebarProvider>
+    </ThemeProvider>
+  );
 }
